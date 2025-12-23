b8310174343dffa18b80eaef12745c51ae49c9a8
Vishal Tiwari <160843582+vishal-tiwari-1971@users.noreply.github.com>
2025-12-24 01:15:49 +0530

    landing page improve
    Removed Navbar and Footer components from layout.

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,9 +1,5 @@
 import "./globals.css";
-
-import Navbar from "@/components/navbar";
-import Footer from "@/components/footer";
 import { GoogleOAuthProvider } from '@react-oauth/google';
-
 
 export const metadata = {
   title: "SPIE-2026",
@@ -13,22 +9,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-
-      <body className="min-h-screen flex flex-col bg-[#0b1220]">
-        
-        {/* Navbar at top */}
-        // <Navbar />
-
-        {/* Main content grows and pushes footer down */}
-        <main className="flex-1">
-           <GoogleOAuthProvider clientId={process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID}>
+      <body>
+        <GoogleOAuthProvider clientId={process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID}>
           {children}
         </GoogleOAuthProvider>
-        </main>
-
-        {/* Footer appears ONLY at end of page */}
-        <Footer />
-
       </body>
     </html>
   );
